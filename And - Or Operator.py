fbae4a7f7b29557880e168ed155595c122b4123e
Parsz123 <hetaldr@yahoo.com>
2025-09-09 18:21:54 +0100

    Hello

---
+++ And - Or Operator.py
@@ -0,0 +1,21 @@
+a = 12
+b = 10
+c = 0
+
+if a and b and c:
+    print("All of the values have true as boolean")
+else:
+    print("At least one of the values has false as boolean")
+
+c = -7
+a = 9
+b = -10
+if a > 0 or b > 0:
+    print("Either of the values are true")
+else:
+    print("None of the values are true")
+
+if b > 0 or c > 0:
+    print("Either of the values are true")
+else:
+    print("None of the values are true")
