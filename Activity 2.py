f852ea62550aedd717421581b454abafb923eeac
Parsz123 <hetaldr@yahoo.com>
2025-09-09 18:38:59 +0100

    hello

---
+++ Activity 2.py
@@ -0,0 +1,17 @@
+a = 12
+b = 10
+c = 10
+print(a!=b)
+print(b!=c)
+a = "Football"
+b = "coding operators"
+if a!=b:
+    print(a, "and", b , "are different")
+
+a = 858
+b = 111
+if (a==1)!=(b==111):
+    print("hello")
+a = int(input("Enter your number"))
+if a%2!=0:
+    print("This is a not even number")
